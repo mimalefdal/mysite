d9b8be924d53708622052ff20a8df11c9a6289f4
mimalefdal <mimalefdal@yahoo.com>
2025-03-24 19:43:19 +0330

    Setup global statics directory

--- site1/urls.py
+++ site1/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from site1.views import *
 
 urlpatterns = [
@@ -9,3 +11,6 @@ urlpatterns = [
     path('api/root/', index_json),
    
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
