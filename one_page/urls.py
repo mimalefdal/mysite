258979e069cd056418dbcccb05d3ec16fc71bea5
mimalefdal <mimalefdal@yahoo.com>
2025-04-05 12:28:51 +0330

    Add static & media urls to onepage app

--- one_page/urls.py
+++ one_page/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from one_page.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'onepage'
 
@@ -8,3 +10,5 @@ urlpatterns = [
    
 ]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
