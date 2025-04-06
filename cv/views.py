43c10223a972c4f4f49a2f1a77b1a68182ca4517
mimalefdal <mimalefdal@yahoo.com>
2025-04-06 22:55:25 +0330

    Accomplish structure & information of cv page

--- cv/views.py
+++ cv/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def index_view(request):
-        return render(request,'cv/index.html')
+
+        data = {
+                "name":"Amin",
+                "surename":"Delvarani",
+                "description":"Software Architect",
+                "age":42,
+        }
+
+        return render(request,'cv/index.html',data)
